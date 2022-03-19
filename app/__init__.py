a8b1aefbd7b69c525c0da2fb2a1198caa1f156e1
Warren1012 <annwar101@gmail.com>
2022-03-19 11:36:08 -0500

    app import error

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True 
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
-
 app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 from app import views
