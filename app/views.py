94cdbec417a203ca2fefccc4fdc3e1aca63bcd76
Warren1012 <annwar101@gmail.com>
2022-03-21 16:56:54 -0500

    error

--- app/views.py
+++ app/views.py
@@ -29,33 +29,31 @@ def about():
 
 @app.route('/property',methods=['POST', 'GET'])
 def addproperty():
-    if request.method == 'POST':
-        form = Propertyform()
-        if form.validate_on_submit(): 
-            
-            photo=form.image.data
-            filename=secure_filename(photo.filename)
-            photo.save(os.path.join(app.confi['UPLOAD_FOLDER'], filename))
-
-
-            title = form.title.data
-            rooms= form.rooms.data
-            bathroom = form.bathroom.data
-            location = form.location.data
-            price = form.price.data
-            type= form.type.data
-            description = form.description.data
-            
-            newProperty = Property(title=title,rooms=rooms,bathroom=bathroom,location=location,price=price,type=type,description=description,image=filename)
-            db.session.add(newProperty)
-            db.session.commit()
-
-            properties =Property.query.all()
-            flash('Success')    
-            return redirect(url_for('/properties',properties=properties))
-        else:
-            flash('Error.Try again','Failed')
-        return render_template('form.html',form=form)
+   form = Propertyform() 
+   if request.method == 'POST':
+       if form.validate_on_submit(): 
+           photo=form.image.data
+           filename=secure_filename(photo.filename)
+           photo.save(os.path.join(app.confi['UPLOAD_FOLDER'], filename))
+           
+           title = form.title.data
+           rooms= form.rooms.data
+           bathroom = form.bathroom.data
+           location = form.location.data
+           price = form.price.data
+           type= form.type.data
+           description = form.description.data
+           
+           newProperty = Property(title=title,rooms=rooms,bathroom=bathroom,location=location,price=price,type=type,description=description,image=filename)
+           db.session.add(newProperty)
+           db.session.commit()
+           
+           properties =Property.query.all()
+           flash('Success')
+           return redirect(url_for('/properties',properties=properties))
+       else:
+           flash('Error.Try again','Failed')
+       return render_template('form.html',form=form)
 
 @app.route('/properties',methods=['POST', 'GET'])
 def properties():
